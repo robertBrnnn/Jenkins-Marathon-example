54b542d865654939d124e92e3c245e9fe59cbd9f
Administrator <robert.brnnn@gmail.com>
2015-12-07 16:56:08 +0000

    Changed json file reference to project specific reference

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,8 @@ curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/ap
 sleep 1
 
 # these lines will create a copy of app_marathon.json and update the image version
-cp -f app_marathon.json app_marathon.json.tmp
-sed -i "s/latest/${version}/g" app_marathon.json.tmp
+cp -f deployment.json deployment.json.tmp
+sed -i "s/latest/${version}/g" deployment.json.tmp
 
 # post the application to Marathon
-curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@app_marathon.json.tmp
+curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@deployment.json.tmp
